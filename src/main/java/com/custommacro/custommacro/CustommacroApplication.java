02b2e0c6e484a08200b3c5494ef822b670e7787a
lchyeong <lchyeong@gmail.com>
2024-10-28 01:16:25 +0900

    Initial commit

---
+++ src/main/java/com/custommacro/custommacro/CustommacroApplication.java
@@ -0,0 +1,13 @@
+package com.custommacro.custommacro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustommacroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustommacroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/custommacro/custommacro/CustommacroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.custommacro.custommacro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustommacroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
